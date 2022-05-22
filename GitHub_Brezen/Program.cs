d8dbef08ee757cb34e4765571da67395e60d8b93
elmic <elmic@DESKTOP-QUPQPGL>
2022-05-22 10:52:01 +0200

    Přidejte soubory projektu.

---
+++ GitHub_Brezen/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int cislo1 = int.Parse(Console.ReadLine());
+int cislo2 = int.Parse(Console.ReadLine());
+Console.Clear();
+
+if (cislo1 < cislo2)
+{
+    Console.WriteLine($"{cislo1},{cislo2} ");
+}
+else
+{
+    Console.WriteLine($"{cislo2},{cislo1} ");
+}
+
